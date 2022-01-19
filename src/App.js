d15137e1891ad0082acb0d63ccd6297e8ebdaa6d
Salomão Pires <salomaomdm@gmail.com>
2022-01-18 21:01:40 -0300

    Adding the Tmbd.js to get all movies from the API

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      Olá mundo!
+    </div>
+  );
+}
+
+
+// https://api.themoviedb.org/3/movie/550?api_key=32c7c1be7aa00b2bc275f61fb0aba6a3&language=pt-BR
---
+++ src/Tmdb.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+/*
+
+** Vamos pegar
+-- Originais da Netflix
+-- Recomendados (trending)\
+-- Em alta (top rated)
+-- Ação
+-- Comédia
+-- Terror
+-- Romance
+-- Documentários 
+*/
+
+
+const API_KEY = "32c7c1be7aa00b2bc275f61fb0aba6a3";
+const API_LANG = "language=pt-BR";
+const API_BASE = "https://api.themoviedb.org/3";
+
+const basicFetch = async (endpoint) => {
+    
+    const url = `${API_BASE}${endpoint}${API_LANG}&${API_KEY}`;
+
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+
+    getHomeList: async () => {
+        return [
+            {
+                slug : "originals",
+                title : "Originais da Netflix",
+                items: await basicFetch(`/discover/tv?with_network=213&`)
+            },
+            {
+                slug : "trending",
+                title : "Recomendados para Você",
+                items: await basicFetch("/trending/all/week?")
+            },
+            {
+                slug : "top rated",
+                title : "Em alta",
+                items: await basicFetch("/movie/top_rated?")
+            },
+            {
+                slug : "action",
+                title : "Ação",
+                items: await basicFetch("discover/movie?with_genres=28&")
+            },
+            {
+                slug : "comedy",
+                title : "Comédia",
+                items: await basicFetch("discover/movie?with_genres=35&")
+            },
+            {
+                slug : "horror",
+                title : "Terror",
+                items: await basicFetch("discover/movie?with_genres=27&")
+            },
+            {
+                slug : "romance",
+                title : "Romance",
+                items: await basicFetch("discover/movie?with_genres=10749&")
+            },
+            {
+                slug : "documentary",
+                title : "Documentários",
+                items: await basicFetch("discover/movie?with_genres=99&")
+            }
+        ]
+    }
+}
