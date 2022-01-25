2c38200051b140a17cab30272837cc4df469fb52
Salomão Pires <salomaomdm@gmail.com>
2022-01-25 07:09:10 -0300

    add loading gif

--- src/App.js
+++ src/App.js
@@ -62,7 +62,15 @@ export default () => {
         ))}
       </section>
       <Footer/>
+
+      {movieList.length <= 0 && 
+        <div className="loading">
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"></img>
+        </div>
+      }
+      
     </div>
+
   );
 }
 
