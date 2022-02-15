df60ffe2f679939a6b3c2fb1f44a616085d710f9
Salomão Pires <salomaomdm@gmail.com>
2022-02-15 15:08:26 -0300

    Add video to show what is the project.

--- src/components/featureMovie/FeatureMovie.js
+++ src/components/featureMovie/FeatureMovie.js
@@ -1,43 +1,53 @@
 import React from "react";
 import "./FeatureMovie.css";
 
+export default ({ item }) => {
+  let firstDate = new Date(item.first_air_date);
+  let genres = [];
 
-export default ({item}) => {
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
 
-    let firstDate = new Date(item.first_air_date);
-    let genres = [];
-    
+  let description = item.overview;
+  if (description.length > 200) {
+    description = description.substring(0, 200) + "...";
+  }
 
-    for(let i in item.genres) {
-        genres.push(item.genres[i].name);
-    }
-    
-    let description = item.overview;
-    if (description.length > 200) {
-        description = description.substring(0, 200) + '...';
-    }
-
-    return (
-        <section className="featured" style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-        }}>
-            <div className="featured--vertical">
-                <div className="featured--horizontal">
-                    <div className="featured--name"> {item.original_name}</div>
-                    <div className="featured--info">
-                        <div className="featured--points"> {item.vote_average} pontos</div>
-                        <div className="featured--year">{firstDate.getFullYear()}</div>
-                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's'  : ''}</div>
-                    </div>
-
-                    <div className="featured--description">{description}</div>
-                    <div className="featured--buttons">
-                        <a href={`watch/${item.id}`} className="feature--watchbutton"> Assistir</a>
-                        <a href={`watch/${item.id}`} className="feature--mylistbutton">+ Minha lista</a>
-                    </div>
-                    <div className="featured--genres"><strong>Generos:</strong> {genres.join(", ")}</div>
-                </div>
+  return (
+    <section
+      className="featured"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name"> {item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points"> {item.vote_average} pontos</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons">
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 ? "s" : ""}
             </div>
-        </section>
-    );
-}
+          </div>
+
+          <div className="featured--description">{description}</div>
+          <div className="featured--buttons">
+            <a href={`watch/${item.id}`} className="feature--watchbutton">
+              {" "}
+              Assistir
+            </a>
+            <a href={`watch/${item.id}`} className="feature--mylistbutton">
+              + Minha lista
+            </a>
+          </div>
+          <div className="featured--genres">
+            <strong>Generos:</strong> {genres.join(", ")}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
